1376ae10b9e3d226d75378b8cdc3bc449689c3fd
Shen Yi Hong <shen_yi_hong@outlook.com>
2025-07-09 15:38:31 +0800

    nvim(telescope): add treesitter, buffers, diagnostics & colorscheme pickers

--- dot_config/nvim/lua/config/plugins/telescope.lua
+++ dot_config/nvim/lua/config/plugins/telescope.lua
@@ -6,8 +6,21 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    require("telescope").setup {
+      defaults = {
+        path_display = { "smart" },
+        layout_config = {
+          horizontal = { prompt_position = "top" },
+        }
+      },
+    }
+
     local builtin = require("telescope.builtin")
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { silent = true })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { silent = true })
+    vim.keymap.set('n', '<leader>ff', builtin.find_files)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers)
+    vim.keymap.set('n', '<leader>ft', builtin.treesitter)
+    vim.keymap.set('n', '<leader>fd', builtin.diagnostics)
+    vim.keymap.set('n', '<leader>cc', builtin.colorscheme)
   end
 }
